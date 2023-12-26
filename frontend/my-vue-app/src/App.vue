7b14e09e644959f28370ff9129287d791c877c35
JikoSchnee <104906844+JikoSchnee@users.noreply.github.com>
2023-12-26 18:25:04 +0800

    Version: 2023/12/16 18:24
    Fancier interface.

--- frontend/my-vue-app/src/App.vue
+++ frontend/my-vue-app/src/App.vue
@@ -1,14 +1,47 @@
-<template>
+<template xmlns:el-col="http://www.w3.org/1999/html">
     <div id="app">
         <h1>Stack Overflow Data Visualization</h1>
         <h3>Topic Popularity</h3>
-        <el-button @click="reloadTagPopularity">Reload Topic Popularity</el-button>
+        <el-button type="primary" @click="reloadTagPopularity" circle plain><i class="el-icon-refresh"></i></el-button>
         <TopicPopularity ref="tagPopularityRef" />
         <h3>Bug Popularity</h3>
-        <el-button @click="reloadBugPopularity2">Reload Bug Popularity</el-button>
-        <BugPopularity2 ref="bugPopularityRef2"></BugPopularity2>
-        <el-button @click="reloadBugPopularity">Reload Bug Popularity</el-button>
-        <BugPopularity ref="bugPopularityRef"></BugPopularity>
+
+
+        <el-row justify="center" gutter="0">
+            <el-button-group>
+                <el-button type="primary" @click = "BetweenOnly" plain>Between</el-button>
+                <el-button type="primary" @click = "SyntaxOnly" plain>Syntax Errors</el-button>
+                <el-button type="primary" @click = "FatalOnly" plain>Fatal Errors</el-button>
+                <el-button type="primary" @click = "ExceptOnly" plain>Exceptions</el-button>
+                <el-button type="primary" @click = "AllErrors" plain>All</el-button>
+            </el-button-group>
+        </el-row>
+        <div v-if="this.isBugPopDefault">
+            <el-button type="primary" circle plain><i class="el-icon-refresh"></i></el-button>
+            <BugPopDefault></BugPopDefault>
+        </div>
+        <div v-if="this.isBugPopularityBetweenVisible">
+            <el-button type="primary" @click="reloadBugPopularity2" circle plain><i class="el-icon-refresh"></i></el-button>
+            <BugPopularity2 ref="bugPopularityRef2"></BugPopularity2>
+        </div>
+        <div v-if="this.isBugPopularitySyntaxVisible">
+            <el-button type="primary" @click="reloadBugPopularitySyntax" circle plain><i class="el-icon-refresh"></i></el-button>
+            <BugPopularitySyntax ref="bugPopularityRefSyntax"></BugPopularitySyntax>
+        </div>
+        <div v-if="this.isBugPopularityFatalVisible">
+            <el-button type="primary" @click="reloadBugPopularityFatal" circle plain><i class="el-icon-refresh"></i></el-button>
+            <BugPopularityFatal ref="bugPopularityRefFatal"></BugPopularityFatal>
+        </div>
+        <div v-if="this.isBugPopularityExceptVisible">
+            <el-button type="primary" @click="reloadBugPopularityExcept" circle plain><i class="el-icon-refresh"></i></el-button>
+            <BugPopularityExcept ref="bugPopularityRefExcept"></BugPopularityExcept>
+        </div>
+        <div v-if="this.isBugPopularityVisible">
+            <el-button type="primary" @click="reloadBugPopularity" circle plain><i class="el-icon-refresh"></i></el-button>
+            <BugPopularity ref="bugPopularityRef"></BugPopularity>
+        </div>
+
+
 
         <H3>Related Topic</H3>
         <TopicWordCloud></TopicWordCloud>
@@ -17,21 +50,37 @@
 </template>
 
 <script>
-import TopicPopularity from "@/components/QueryBoxComponents/TopicPopularity.vue";
-import BugPopularity from "@/components/QueryBoxComponents/BugPopularity.vue";
+import TopicPopularity from "@/components/TopicPopCompo/TopicPopularity.vue";
 import TopicWordCloud from "@/components/QueryBoxComponents/TopicWordCloud.vue";
-import BugPopularity2 from "@/components/QueryBoxComponents/BugPopularity2.vue";
+import BugPopularity from "@/components/BugPopCompo/BugPopularity.vue";
+import BugPopularity2 from "@/components/BugPopCompo/BugPopularity2.vue";
+import BugPopularitySyntax from "@/components/BugPopCompo/BugPopularitySyntax.vue";
+import BugPopularityFatal from "@/components/BugPopCompo/BugPopularityFatal.vue";
+import BugPopularityExcept from "@/components/BugPopCompo/BugPopularityExcept.vue";
+import BugPopDefault from "@/components/BugPopCompo/BugPopDefault.vue";
 
 export default {
+
+
     name: 'App',
     components: {
         TopicWordCloud,
         TopicPopularity,
         BugPopularity,
-        BugPopularity2
+        BugPopularity2,
+        BugPopularitySyntax,
+        BugPopularityFatal,
+        BugPopularityExcept,
+        BugPopDefault
     },
     data() {
         return {
+            isBugPopDefault: true,
+            isBugPopularityBetweenVisible: false,
+            isBugPopularityVisible: false,
+            isBugPopularitySyntaxVisible: false,
+            isBugPopularityFatalVisible: false,
+            isBugPopularityExceptVisible: false,
 
         };
     },
@@ -45,7 +94,56 @@ export default {
         },
         reloadBugPopularity2() {
             this.$refs.bugPopularityRef2.generateChart();
-        }
+        },
+        reloadBugPopularitySyntax() {
+            this.$refs.bugPopularityRefSyntax.generateChart();
+        },
+        reloadBugPopularityFatal() {
+            this.$refs.bugPopularityRefFatal.generateChart();
+        },
+        reloadBugPopularityExcept() {
+            this.$refs.bugPopularityRefExcept.generateChart();
+        },
+        BetweenOnly() {
+            this.isBugPopDefault = false;
+            this.isBugPopularityBetweenVisible = true;
+            this.isBugPopularitySyntaxVisible = false;
+            this.isBugPopularityFatalVisible = false;
+            this.isBugPopularityExceptVisible = false;
+            this.isBugPopularityVisible = false;
+        },
+        SyntaxOnly() {
+            this.isBugPopDefault = false;
+            this.isBugPopularityBetweenVisible = false;
+            this.isBugPopularitySyntaxVisible = true;
+            this.isBugPopularityFatalVisible = false;
+            this.isBugPopularityExceptVisible = false;
+            this.isBugPopularityVisible = false;
+        },
+        FatalOnly() {
+            this.isBugPopDefault = false;
+            this.isBugPopularityBetweenVisible = false;
+            this.isBugPopularitySyntaxVisible = false;
+            this.isBugPopularityFatalVisible = true;
+            this.isBugPopularityExceptVisible = false;
+            this.isBugPopularityVisible = false;
+        },
+        ExceptOnly() {
+            this.isBugPopDefault = false;
+            this.isBugPopularityBetweenVisible = false;
+            this.isBugPopularitySyntaxVisible = false;
+            this.isBugPopularityFatalVisible = false;
+            this.isBugPopularityExceptVisible = true;
+            this.isBugPopularityVisible = false;
+        },
+        AllErrors() {
+            this.isBugPopDefault = false;
+            this.isBugPopularityBetweenVisible = false;
+            this.isBugPopularitySyntaxVisible = false;
+            this.isBugPopularityFatalVisible = false;
+            this.isBugPopularityExceptVisible = false;
+            this.isBugPopularityVisible = true;
+        },
     }
 };
 </script>
---
+++ frontend/my-vue-app/src/components/BugPopCompo/BugPopularitySyntax.vue
@@ -0,0 +1,164 @@
+<template>
+    <div>
+        <!--        <h2>Bug Popularity</h2>-->
+        <el-row>
+            <!-- el-transfer -->
+            <el-col :span="12" >
+
+                <el-transfer v-model="value" :data="data" :titles="['Categories', 'Chosen']" class="transfer">
+                    <!-- Add margin to the label inside the transfer component -->
+                    <template slot="left-footer" slot-scope="{ data }">
+                        <div v-for="item in data" :key="item.key" class="transfer-item">
+                            <el-checkbox :label="item.key" class="checkbox">{{ item.label }}</el-checkbox>
+                        </div>
+                    </template>
+                </el-transfer>
+            </el-col>
+            <!-- Chart container -->
+            <el-col :span="12">
+                <div class="chart-container">
+                    <div ref="echartsContainer" class="chart"></div>
+                </div>
+            </el-col>
+        </el-row>
+    </div>
+</template>
+
+<script>
+import * as echarts from "echarts";
+
+export default {
+    data() {
+        this.words = [
+            //SyntaxError
+            'IndentationError', 'SyntaxError',
+        ];
+        this.keyword = [
+            'IndentationError', 'SyntaxError',
+        ]
+        const generateData = () => {
+            const data = [];
+            for (let i = 1; i <= this.words.length; i++) {
+                data.push({
+                    key: i,
+                    label: this.words[i - 1],
+                    // disabled: i % 4 === 0,
+                });
+            }
+            return data;
+        };
+
+        return {
+            data: generateData(),
+            value: [1,2],
+            pieChartData : []
+        };
+    },
+    mounted() {
+        this.generateChart();
+    },
+    methods: {
+        generateChart() {
+            console.log('generate pie chart')
+            // console.log(this.value)
+            if (this.myChart) {
+                this.myChart.dispose();
+            }
+            this.pieChartData = [];
+            const requests = this.value.map((val) => {
+                const str = this.keyword[val - 1];
+                return this.request.get('/api/question/count/keyword/' + str)
+                    .then(count => {
+                        // console.log('------this.value------');
+                        // console.log(val);
+                        // console.log(this.words[val - 1]);
+                        // console.log(count);
+                        this.pieChartData.push({ value: count, name: this.words[val - 1] });
+                    });
+            });
+
+            Promise.all(requests)
+                .then(() => {
+                    this.$nextTick(() => {
+                        this.myChart = echarts.init(this.$refs.echartsContainer);
+                        this.myChart.setOption({
+                            tooltip: {
+                                trigger: 'item'
+                            },
+                            legend: {
+                                top: '5%',
+                                left: 'center'
+                            },
+                            series: [
+                                {
+                                    name: 'Bug Popularity',
+                                    type: 'pie',
+                                    radius: ['40%', '70%'],
+                                    avoidLabelOverlap: false,
+                                    itemStyle: {
+                                        borderRadius: 6,
+                                        borderColor: '#e5e9f2',
+                                        borderWidth: 3
+                                    },
+                                    label: {
+                                        show: false,
+                                        position: 'center'
+                                    },
+                                    emphasis: {
+                                        label: {
+                                            show: true,
+                                            fontSize: 40,
+                                            fontWeight: 'bold'
+                                        }
+                                    },
+                                    labelLine: {
+                                        show: false
+                                    },
+                                    data: this.pieChartData
+                                }
+                            ]
+                        });
+                    });
+                });
+        }
+    }
+};
+</script>
+
+<style scoped>
+h2 {
+    margin-bottom: 20px;
+}
+
+.el-row {
+    margin-bottom: 20px;
+}
+
+.transfer-item {
+    margin-bottom: 8px; /* Adjust the margin as needed */
+}
+
+.checkbox {
+    margin-left: 0
+}
+
+.transfer {
+    text-align: left;
+}
+
+.chart-container {
+    box-sizing: border-box;
+    flex: 1;
+    margin: 0 5px;
+
+}
+
+.chart {
+    width: 100%;
+    height: 300px;
+    background: #eceff3;
+    border-radius: 10px;
+}
+</style>
+/* Add more styling
+
